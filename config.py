ed81d45ae229352805ce92aa5fc3c874eaf0e76b
vlavsa <vlavsa@yandex.ru>
2025-03-14 22:22:59 +0300

    Added output of the response result to the user

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ API_TOKEN = "7655503221:AAFHEp9YpuPpZRJGL6wvZWK2_RI-pRJTSR8"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_NAME = os.path.join(BASE_DIR, "quize_data.db")
+QUIZ_BUTTON = "Начать опрос"
